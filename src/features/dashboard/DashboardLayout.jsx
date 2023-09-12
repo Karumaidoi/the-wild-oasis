1d6753625b96c1ef78855e5c76606073e89c2532
Karumaidoi <mainahmwangi12@gmail.com>
2023-09-12 17:03:47 +0300

    Computing recent booking and Stays - error -- on progress

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,24 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { data, isLoading: loadingBookings } = useRecentBookings();
+  const { confirmedStays, isLoading: loadingStays } = useRecentStays();
+
+  if (loadingBookings || loadingStays) return <Spinner />;
+
+  console.log(confirmedStays);
+
+  console.log(data);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays Activities</div>
+      <div>Chart stay duration</div>
+      <div>Chart of sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -53,10 +53,12 @@ export async function getBookings({ filter, sortBy, page }) {
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
-    .from("bookings")
-    .select("created_at, totalPrice, extrasPrice")
+    .from("Bookings")
+    .select("created_at, totalPrice, extraPrice")
     .gte("created_at", date)
     .lte("created_at", getToday({ end: true }));
+
+  console.log(data);
 
   if (error) {
     console.error(error);
@@ -69,14 +71,14 @@ export async function getBookingsAfterDate(date) {
 // Returns all STAYS that are were created after the given date
 export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
-    .from("bookings")
+    .from("Bookings")
     // .select('*')
-    .select("*, guests(fullName)")
+    .select("*, Guest(fullName)")
     .gte("startDate", date)
-    .lte("startDate", getToday());
+    .lte("startDate", getToday({ end: true }));
 
   if (error) {
-    console.error(error);
+    console.error(error.message);
     throw new Error("Bookings could not get loaded");
   }
 
@@ -86,7 +88,7 @@ export async function getStaysAfterDate(date) {
 // Activity means that there is a check in or a check out today
 export async function getStaysTodayActivity() {
   const { data, error } = await supabase
-    .from("bookings")
+    .from("Bookings")
     .select("*, guests(fullName, nationality, countryFlag)")
     .or(
       `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
