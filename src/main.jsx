47ab3de159ef75208f5478d2f074a685bb03cfc7
Karumaidoi <mainahmwangi12@gmail.com>
2023-09-13 12:42:22 +0300

    Error Boundary - done

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DarkModeProvider>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => window.location.replace("/")}
+      >
+        <App />
+      </ErrorBoundary>
     </DarkModeProvider>
   </React.StrictMode>
 );
